02593a8c4cbc6315e49f096e1aaa89a8a05b215d
rahul-a-bangera <rahul.bangera.999@gmail.com>
2026-01-14 03:02:00 +0530

    fix: Use environment.apiUrl in resume service

--- PortfolioFrontend/src/app/services/resume.service.ts
+++ PortfolioFrontend/src/app/services/resume.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResumeData } from '../models/resume.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResumeService {
-  private apiUrl = 'http://localhost:5091/api/resume';
+  private apiUrl = `${environment.apiUrl}/api/resume`;
 
   constructor(private http: HttpClient) { }
 
