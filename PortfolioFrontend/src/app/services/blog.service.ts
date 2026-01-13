a17c22e3a6f65933fb32bbc69eac4e7ac33ade1f
rahul-a-bangera <rahul.bangera.999@gmail.com>
2026-01-14 03:05:33 +0530

    fix: Use environment.apiUrl in all services - Remove all hardcoded localhost references

--- PortfolioFrontend/src/app/services/blog.service.ts
+++ PortfolioFrontend/src/app/services/blog.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../models/blog-post.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  private apiUrl = 'http://localhost:5091/api/blog';
+  private apiUrl = `${environment.apiUrl}/api/blog`;
 
   constructor(private http: HttpClient) { }
 
